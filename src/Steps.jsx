98220a7118b227fec58d624e8b333212c928ce3c
roxxe3 <hamzafarissi@gmail.com>
2025-01-26 21:41:45 +0100

    update Steps component layout; adjust spacing and enhance step descriptions

--- src/Steps.jsx
+++ src/Steps.jsx
@@ -2,19 +2,19 @@ import pac from './assets/Ellipse 17.svg'
 export default function Steps() {
     return (
         <div className='flex'>
-            <div className='flex flex-col w-0 mx-10 items-center '>
+            <div className='flex flex-col w-0 mx-14 items-center '>
                 <div className="border-2 rounded-full w-6 h-6 p-5 flex items-center justify-center m-4 mt-0">
                     <div className="text-white">1</div>
                 </div>
-                <div className='h-37 border border-white/20'></div>
+                <div className='h-43 border border-white/20'></div>
                 <div className="border-2 rounded-full w-6 h-6 p-5 flex items-center justify-center m-4 ">
                     <div className="text-white">2</div>
                 </div>
-                <div className='h-30 border border-white/20'></div>
+                <div className='h-35 border border-white/20'></div>
                 <div className="border-2 rounded-full w-6 h-6 p-5 flex items-center justify-center m-4 ">
                     <div className="text-white">3</div>
                 </div>
-                <div className='h-31 border border-white/20'></div>
+                <div className='h-35 border border-white/20'></div>
                 <div className="border-2 rounded-full w-6 h-6 p-5 flex items-center justify-center m-4 ">
                     <div className="text-white">4</div>
                 </div>
@@ -22,17 +22,17 @@ export default function Steps() {
             </div>
             <div>
 
-                <div className='flex flex-col justify-between gap-y-4 mb-5'>
+                <div className='flex flex-col justify-between gap-y-4 mb-10'>
                     <img className='w-10' src={pac} alt="pacman" />
                     <h2 className='text-2xl'>Step 1: Requirement Analysis</h2>
                     <p className='text-white/80 max-w-110 md:max-w-170 '>I begin by understanding project goals, defining system architecture, and gathering technical requirements to lay a strong foundation for both backend and frontend development.</p>
                 </div>
-                <div className='flex flex-col justify-between gap-y-4 mb-5'>
+                <div className='flex flex-col justify-between gap-y-4 mb-10'>
                     <img className='w-10 rotate-90' src={pac} alt="pacman" />
                     <h2 className='text-2xl'>Step 2: Backend Development</h2>
                     <p className='text-white/80 max-w-110 md:max-w-170 '>I build secure, scalable APIs and optimize server-side logic using technologies like Python, C++, or Node.js, ensuring performance and reliability.</p>
                 </div>
-                <div className='flex flex-col justify-between gap-y-4 mb-5'>
+                <div className='flex flex-col justify-between gap-y-4 mb-10'>
                     <img className='w-10 rotate-180' src={pac} alt="pacman" />
                     <h2 className='text-2xl'>Step 3: Frontend Development</h2>
                     <p className='text-white/80 max-w-110 md:max-w-170'>I design and implement responsive user interfaces that integrate seamlessly with the backend, focusing on creating intuitive and engaging user experiences.</p>
