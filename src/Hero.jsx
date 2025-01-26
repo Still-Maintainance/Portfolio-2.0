001fc0170925c629479b53a1bd69c9bb9225018c
roxxe3 <hamzafarissi@gmail.com>
2025-01-26 15:33:36 +0100

    enhance Header and Hero components with improved styling and layout; add SVG assets for visuals

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,27 +1,40 @@
+import portal from './assets/Portal.svg';
+import vector from './assets/Vector.svg';
+
 export default function Hero() {
     return (
-        <div className="mt-10 grid grid-cols-4 grid-rows-8 opacity-20">
-            <div className="border-[0.05px] h-20">01</div>
-            <div className="border-[0.05px] h-20">02</div>
-            <div className="border-[0.05px] h-20">03</div>
-            <div className="border-[0.05px] h-20">04</div>
-            <div className="flex justify-center items-center col-start-1 col-end-5 row-span-5">
-                <h1>I design and build clean websites</h1>
+        <>
+        <div className="mt-15 grid grid-cols-4 grid-rows-8 relative">
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="flex flex-wrap flex-col justify-center text-center items-center col-start-1 col-end-5 row-span-5">
+                <h1 className="text-3xl">I design and build clean websites</h1>
+                <p className="text-base my-5 mx-10">Write anything here something about yourself to showcase what actually you doing or targeting etc.</p>
+                <div className="p-[2px] rounded-full bg-gradient-to-r from-[#FFD49C] to-[#7A87FB]">
+                    <button className="p-3 px-6 font-semibold rounded-full bg-gradient-to-r from-[#7A87FB] to-[#FFD49C] text-[#101111]">Hire me</button>
                 </div>
-            <div className="border-[0.05px] h-20">06</div>
-            <div className="border-[0.05px] h-20">07</div>
-            <div className="border-[0.05px] h-20">08</div>
-            <div className="border-[0.05px] h-20">09</div>
-            <div className="border-[0.05px] h-20">10</div>
-            <div className="border-[0.05px] h-20">11</div>
-            <div className="border-[0.05px] h-20">12</div>
-            <div className="border-[0.05px] h-20">13</div>
-            <div className="border-[0.05px] h-20">14</div>
-            <div className="border-[0.05px] h-20">15</div>
-            <div className="border-[0.05px] h-20">16</div>
-            <div className="border-[0.05px] h-20">17</div>
-
-
+            </div>
+            <img src={portal} alt="Portal" className="w-7 absolute top-16 left-1/4 transform -translate-x-1/2"/>
+            <img src={portal} alt="Portal" className="w-7 absolute bottom-36.5 left-3/4 transform -translate-x-1/2"/>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
+            <div className="border-[0.5px] h-20 opacity-20"></div>
         </div>
+        <div className='flex justify-between items-center gap-x-10 p-10 border-b-2 border-white/20'>
+            <h1 className='text-xl'>Figma</h1>
+            <img className='w-8' src={vector} alt="Vector" />
+            <h1 className='text-xl'>AWS</h1>
+            <img className='w-8' src={vector} alt="Vector" />
+            <h1 className='text-xl'>React</h1>
+            <img className='w-8' src={vector} alt="Vector" />
+        </div>
+        </>
     );
 }
