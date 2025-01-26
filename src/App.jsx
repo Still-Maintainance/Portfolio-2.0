a7a4c10cba2fa603af2a7af189b39456c9d823d0
roxxe3 <hamzafarissi@gmail.com>
2025-01-26 19:11:42 +0100

    add Steps component with SVG asset; update App to include Steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Hero from "./Hero";
-
+import Steps from "./Steps";
 export default function App() {
     return (
         <div>
             <Header />
             <Hero />
+            <Steps />
         </div>
     );
 }
---
+++ src/Steps.jsx
@@ -0,0 +1,7 @@
+import pac from './assets/Ellipse 17.svg'
+export default function Steps() {
+    return (
+        <div>
+            <img src={pac} alt="pacman" />
+        </div>
+);}
