a899d25d58ba6d7711027d59ac863ba0cbcabe3f
roxxe3 <hamzafarissi@gmail.com>
2025-01-29 13:17:08 +0100

    refactor Testimonial component for improved layout; adjust padding for better visual consistency

--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -3,9 +3,9 @@ import fasila from "./assets/fasila.svg";
 // eslint-disable-next-line react/prop-types
 export default function Testimonial({ name, title, feedback }) {
   return (
-    <div className="relative mx-auto mb-7 max-w-md sm:max-w-xs">
+    <div className="relative mx-auto mb-7 max-w-xs md:max-w-md">
       <div className="rounded-3xl bg-gradient-to-r from-[#FFD49C] to-[#7A87FB] p-[1px]">
-        <div className="rounded-3xl bg-[#151515] p-4">
+        <div className="rounded-3xl bg-[#151515] p-6">
           <div className="mb-6 flex items-center">
             <div className="flex w-full items-center justify-between">
               <div className="flex items-center">
@@ -13,7 +13,7 @@ export default function Testimonial({ name, title, feedback }) {
                   <img />
                 </div>
                 <div className="ml-4">
-                  <h3 className="text-lg font-medium text-white">{name}</h3>
+                  <h3 className="text-lg text-white">{name}</h3>
                   <p className="text-sm text-gray-400">{title}</p>
                 </div>
               </div>
