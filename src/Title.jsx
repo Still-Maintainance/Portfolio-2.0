557de051762e7f01fe64a3b1f377a28fc0858a1c
roxxe3 <hamzafarissi@gmail.com>
2025-01-29 13:20:44 +0100

    refactor Title component for improved layout; adjust image sizes for better alignment

--- src/Title.jsx
+++ src/Title.jsx
@@ -6,8 +6,11 @@ export default function Title({ title }) {
     <div className="mt-12 mb-12 ml-6 flex items-center">
       <h1 className="mr-3 text-[1.3rem] md:text-3xl">{title}</h1>
       <div className="relative">
-        <img src={portal} className="w-7 md:w-10" />
-        <img src={portal} className="absolute bottom-7 left-7 w-3 md:w-4" />
+        <img src={portal} className="w-6 md:w-10" />
+        <img
+          src={portal}
+          className="absolute bottom-5 left-5 w-2.5 md:bottom-7 md:left-7 md:w-4"
+        />
       </div>
     </div>
   );
