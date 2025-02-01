9c1b820ed97612099ebc7ec9ab66f16ba679965b
roxxe3 <hamzafarissi@gmail.com>
2025-02-01 15:13:14 +0100

    refactor Contact and Title components; enhance layout, spacing, and responsiveness

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,13 +9,13 @@ import Title from "./Title";
 
 export default function Contact() {
   return (
-    <div className="text-text-sm mx-auto flex w-13/14 flex-col items-start justify-start gap-5 lg:mx-auto lg:w-15/20">
+    <div className="text-text-sm mx-auto flex w-13/14 flex-col items-start justify-start gap-5 lg:mx-auto lg:w-15/20 lg:mt-20">
       <div className="flex justify-between w-15/20">
         <div className="flex flex-col lg:flex-row lg:justify-between">
-          <div className="flex flex-col">
-            <Title title="Have idea about project?" className={"ml-0"}/>
+          <div className="flex flex-col lg:w-150">
+            <Title title="Have idea about project?" className={"mt-12 ml-0 flex items-center md:w-15/20 mb-12"}/>
             <div className="flex flex-col items-start justify-start gap-5">
-              <p className="lg:max-w-100 mt-12">
+              <p className="lg:max-w-100 mt-4 mb-5">
                 I&apos;d love to hear from you! Feel free to reach out for
                 questions, collaboration, or just to connect.
               </p>
--- src/Title.jsx
+++ src/Title.jsx
@@ -3,7 +3,7 @@ import portal from "./assets/Portal.svg";
 // eslint-disable-next-line react/prop-types
 export default function Title({ title, className }) {
   return (
-    <div className={`mt-12 ml-6 flex items-center md:w-15/20 md:mx-auto ${className || 'mb-12'}`}>
+    <div className={className ? className :"mt-12 ml-6 flex items-center md:w-15/20 md:mx-auto mb-12" }>
       <h1 className="mr-3 text-[1.3rem] md:text-3xl">{title}</h1>
       <div className="relative">
         <img src={portal} className="w-6 md:w-10" />
